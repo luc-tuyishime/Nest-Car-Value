9b98e413321a6785d96a7ab3c1053e538128cdb5
luc-tuyishime <luctunechi45@gmail.com>
2023-04-20 16:16:43 +0200

    first commit

---
+++ src/users/dtos/create-user.dtos.ts
@@ -0,0 +1,21 @@
+import {
+  IsEmail,
+  MinLength,
+  MaxLength,
+  Matches,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  @IsString()
+  password: string;
+}
